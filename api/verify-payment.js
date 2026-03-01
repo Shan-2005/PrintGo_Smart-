86a6f38231db433b4ed8e8dd19d0cca0b20e3b7e
Shan-2005 <sn0760@srmist.edu.in>
2026-03-01 15:29:47 +0530

    fix: convert vercel serverless functions to ESModules to resolve ReferenceError for require

--- api/verify-payment.js
+++ api/verify-payment.js
@@ -1,6 +1,6 @@
-const crypto = require('crypto');
+import crypto from 'crypto';
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
     // CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
