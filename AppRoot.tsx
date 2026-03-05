9c492b1ea856aeaf68e0977b57875b50828cf86d
Shan-2005 <sn0760@srmist.edu.in>
2026-03-05 18:36:38 +0530

    fix: switch to HashRouter and force relative asset paths for Android startup stability

--- AppRoot.tsx
+++ AppRoot.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import UserPage from './src/pages/UserPage';
 import KioskPage from './src/pages/KioskPage';
 import AndroidKioskPage from './src/pages/AndroidKioskPage';
